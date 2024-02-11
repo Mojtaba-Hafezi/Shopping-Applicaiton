5e9f87b9847ba25ca5c77c0705b0c14247c702e8
Mojtaba Hafezi <mojtaba.hafeze.wien@gmail.com>
2024-02-11 13:38:42 +0100

    First step

---
+++ ShoppingApp/Controllers/CategoriesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingApp.Models;
+
+namespace ShoppingApp.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            return View(new Category { CategoryId = id.HasValue ? id.Value : 0 }); ;
+
+        }
+    }
+}
---
+++ ShoppingApp/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
